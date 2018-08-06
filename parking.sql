a776206f6589c939407f435a08a5b8e9f98bd83d
matthewSkipworth <mfskipwo@uw.edu>
2018-08-06 15:08:14 -0700

    some formatting changes

--- parking.sql
+++ parking.sql
@@ -1,46 +1,44 @@
 create table Lot(lotName varchar(20),
-						  location varchar(20), 
-						  capacity integer, 
-                          floors integer,
-                          primary key(lotName)
-                          );
+						location varchar(20), 
+                        capacity integer, 
+                        floors integer,
+                        primary key(lotName)
+                        );
 
 create table Space(spaceNumber integer,
-					           spaceType varchar(20),
-							   lotName varchar(20), 
-                               primary key(spaceNumber),
-                               foreign key(lotName) references Lot(lotName)
-                               );
-
-create table StaffSpace(staffNumber integer,
-                                       spaceNumber integer, 
-                                       primary key(staffNumber, spaceNumber),
-                                    #   foreign key (staffNumber, spaceNumber),
-                                     #  references (Staff, Space) 
-                                       foreign key (staffNumber) references Staff(staffNumber),
-                                       foreign key (spaceNumber) references Space(spaceNumber)
-                                       );
+								spaceType varchar(20),
+                                lotName varchar(20), 
+                                primary key(spaceNumber),
+                                foreign key(lotName) references Lot(lotName)
+                                );
 
 create table Staff(staffNumber integer,
 							telephoneExt int, 
-						    vehicleLicenseNumber integer,
+                            vehicleLicenseNumber integer,
                             primary key(staffNumber)
                             );
+
+create table StaffSpace(staffNumber integer,
+										spaceNumber integer,
+                                        primary key(staffNumber, spaceNumber),
+                                        foreign key (staffNumber) references Staff(staffNumber),
+                                        foreign key (spaceNumber) references Space(spaceNumber)
+                                        );
                             
 create table SpaceBooking(BookingId integer ,
 											spaceNumber integer,
 											staffNumber integer, 
-                                            visitorLicence integer, 
-                                            dateOfVisit date,
-                                            primary key(BookingId),
-                                            foreign key(spaceNumber) references Space(spaceNumber),
-                                            foreign key (staffNumber) references Staff(staffNumber)
-                                            );
+											visitorLicence integer, 
+											dateOfVisit date,
+											primary key(BookingId),
+											foreign key(spaceNumber) references Space(spaceNumber),
+											foreign key (staffNumber) references Staff(staffNumber)
+											);
 
 create table CoveredSpace(spaceNumber integer,
 											monthlyRate decimal);
 
 create table UncoveredSpace(spaceNumber integer,
-												primary key(spaceNumber), 
+												primary key(spaceNumber),
 												foreign key (spaceNumber) references Space(spaceNumber)
-                                                );                           
+												);                           

47bd90f0291c4dc435bfa052dab30ac2ebd69d72
matthewSkipworth <mfskipwo@uw.edu>
2018-08-09 18:05:59 -0700

    parking.sql updates

--- parking.sql
+++ parking.sql
@@ -5,12 +5,14 @@ create table Lot(lotName varchar(20),
                         primary key(lotName)
                         );
 
-create table Space(spaceNumber integer,
+create table `Space`(spaceNumber integer,
 								spaceType varchar(20),
-                                lotName varchar(20), 
+                                pLotName varchar(20), 
                                 primary key(spaceNumber),
-                                foreign key(lotName) references Lot(lotName)
+                                constraint fk_lotNm foreign key(pLotName) references Lot(lotName)
                                 );
+                                
+
 
 create table Staff(staffNumber integer,
 							telephoneExt int, 
@@ -18,29 +20,27 @@ create table Staff(staffNumber integer,
                             primary key(staffNumber)
                             );
 
-create table StaffSpace(staffNumber integer,
-										spaceNumber integer,
-                                        primary key(staffNumber, spaceNumber),
-                                        foreign key (staffNumber) references Staff(staffNumber),
-                                        foreign key (spaceNumber) references Space(spaceNumber)
+create table StaffSpace(staffNum integer,
+										pSpaceNumber integer,
+                                        primary key(staffNum, pSpaceNumber),
+                                        constraint fk_staffNum foreign key (staffNum) references Staff(staffNumber),
+                                        constraint fk_pSpaceNumber foreign key (pSpaceNumber) references CoveredSpace(spaceNumber)
                                         );
                             
-create table SpaceBooking(BookingId integer ,
-											spaceNumber integer,
-											staffNumber integer, 
-											visitorLicence integer, 
-											dateOfVisit date,
-											primary key(BookingId),
-											foreign key(spaceNumber) references Space(spaceNumber),
-											foreign key (staffNumber) references Staff(staffNumber)
+create table SpaceBooking(BookingId integer, spaceNum integer, staffN integer, visitorLicence integer, dateOfVisit date, primary key(BookingId),
+											constraint fk_spaceNum foreign key(spaceNum) references CoveredSpace(spaceNumber),
+											constraint fk_staffN foreign key (staffN) references Staff(staffNumber)
 											);
 
-create table CoveredSpace(spaceNumber integer,
+create table CoveredSpace(coveredSpaceNumber integer,
 											monthlyRate decimal,
-                                            primary key(spaceNumber),
-                                            foreign key(spaceNumber)references Space(spaceNumber));
+                                            primary key(coveredSpaceNumber),
+                                            constraint fk_coveredSpaceNumber foreign key(coveredSpaceNumber)references `Space`(spaceNumber));
 
-create table UncoveredSpace(spaceNumber integer,
-												primary key(spaceNumber),
-												foreign key (spaceNumber) references Space(spaceNumber)
+create table UncoveredSpace(uncoveredSpaceNumber integer,
+												primary key(uncoveredSpaceNumber),
+												constraint fk_uncoveredSpaceNumber foreign key (uncoveredSpaceNumber) references `Space`(spaceNumber)
 												);                           
+                                                
+                                               
+                                                
